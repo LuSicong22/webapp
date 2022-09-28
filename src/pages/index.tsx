876321df0b4685b7b46da2c1be5a832622c252ee
simon <simon.lu@tron.network>
2022-09-28 14:03:26 +0800

    feat: add connect button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,50 @@ import Head from 'next/head'
 import NextLink from "next/link"
 import { VStack, Heading, Box, LinkOverlay, LinkBox} from "@chakra-ui/layout"
 import { Text, Button } from '@chakra-ui/react'
+import { useState, useEffect} from 'react'
+import {ethers} from "ethers"
+
+declare let window:any
 
 const Home: NextPage = () => {
+  const [balance, setBalance] = useState<string | undefined>()
+  const [currentAccount, setCurrentAccount] = useState<string | undefined>()
+  const [chainId, setChainId] = useState<number | undefined>()
+  const [chainname, setChainName] = useState<string | undefined>()
+
+  useEffect(()=>{
+    if(!currentAccount||!ethers.utils.isAddress(currentAccount)) return
+    if(!window.ethereum) return
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    provider.getBalance(currentAccount).then((result)=>{
+      setBalance(ethers.utils.formatEther(result))
+    })
+    provider.getNetwork().then((result)=>{
+      setChainId(result.chainId)
+      setChainName(result.name)
+    })
+  },[currentAccount])
+
+  const onClickConnect= ()=>{
+    //client side code
+    if(!window.ethereum){
+      console.log("please install MetaMask")
+      return
+    }
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    provider.send("eht_requestAccounts",[])
+    .then((accounts)=>{
+      if(accounts.length>0) setCurrentAccount(accounts[0])
+    })
+    .catch((e)=>console.log(e))
+  }
+
+  const onClickDisconnect = () =>{
+    console.log("onClickDisConnect")
+    setBalance(undefined)
+    setCurrentAccount(undefined)
+  }
+
   return (
     <>
       <Head>
@@ -14,6 +56,24 @@ const Home: NextPage = () => {
 
       <Heading as="h3"  my={4}>Explore Web3</Heading>          
       <VStack>
+        <Box w='100%' my={4}>
+        {currentAccount  
+          ? <Button type="button" w='100%' onClick={onClickDisconnect}>
+                Account:{currentAccount}
+            </Button>
+          : <Button type="button" w='100%' onClick={onClickConnect}>
+                  Connect MetaMask
+              </Button>
+        }
+        </Box>
+        {currentAccount  
+          ?<Box  mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
+          <Heading my={4}  fontSize='xl'>Account info</Heading>
+          <Text>ETH Balance of current account: {balance}</Text>
+          <Text>Chain Info: ChainId {chainId} name {chainname}</Text>
+        </Box>
+        :<></>
+        }
         <Box  my={4} p={4} w='100%' borderWidth="1px" borderRadius="lg">
           <Heading my={4}  fontSize='xl'>Task 1</Heading>
           <Text>local chain with hardhat</Text>
